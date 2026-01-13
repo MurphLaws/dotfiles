b9b500caa01c708c89a16a837ceaac9085379d2f
Nicolas Lasso <illico@illico.local>
2026-01-13 13:38:46 -0500

    perf(which-key): reduce delay for faster menu display

--- nvim/.config/nvim/lua/illico/plugins/which-key.lua
+++ nvim/.config/nvim/lua/illico/plugins/which-key.lua
@@ -1,37 +1,43 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  config = function()
-    local wk = require("which-key")
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function()
+		local wk = require("which-key")
 
-    wk.setup({
-      -- Use helix preset for bottom-right positioning
-      preset = "helix",
-      win = {
-        no_overlap = true,
-        border = "single",
-        padding = { 1, 2 },
-        title = false,
-      },
-      layout = {
-        spacing = 3,
-      },
-      icons = {
-        mappings = true,
-      },
-    })
+		wk.setup({
+			-- Use helix preset for bottom-right positioning
+			preset = "helix",
 
-    -- Register keymap groups
-    wk.add({
-      { "<leader>f", group = "Find" },
-      { "<leader>b", group = "Buffer" },
-      { "<leader>c", group = "Code" },
-      { "<leader>d", group = "Debug" },
-      { "<leader>g", group = "Git" },
-      { "<leader>l", group = "LSP" },
-      { "<leader>s", group = "Search" },
-      { "<leader>t", group = "Toggle" },
-      { "<leader>w", group = "Window" },
-    })
-  end,
+			-- ⚡️ CORRECCIÓN DE VELOCIDAD:
+			-- Establecer el retardo a 0 hace que el menú aparezca instantáneamente.
+			-- Por defecto usa 'timeoutlen' (300ms), lo que causa la sensación de lentitud.
+			delay = 0,
+
+			win = {
+				no_overlap = true,
+				border = "single",
+				padding = { 1, 2 },
+				title = false,
+			},
+			layout = {
+				spacing = 3,
+			},
+			icons = {
+				mappings = true,
+			},
+		})
+
+		-- Register keymap groups
+		wk.add({
+			{ "<leader>f", group = "Find" },
+			{ "<leader>b", group = "Buffer" },
+			{ "<leader>c", group = "Code" },
+			{ "<leader>d", group = "Debug" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>l", group = "LSP" },
+			{ "<leader>s", group = "Search" },
+			{ "<leader>t", group = "Toggle" },
+			{ "<leader>w", group = "Window" },
+		})
+	end,
 }
