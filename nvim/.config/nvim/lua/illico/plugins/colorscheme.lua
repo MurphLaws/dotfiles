80ce3c400a64ef447edd5f5a13330aca3d1cafe2
Nicolas Lasso <illico@illico.local>
2026-01-13 14:03:07 -0500

    refactor: switch to 0x96f theme for ghostty and nvim

--- nvim/.config/nvim/lua/illico/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/illico/plugins/colorscheme.lua
@@ -1,92 +1,33 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
+		"filipjanevski/0x96f.nvim",
 		priority = 1000,
-		opts = {
-			flavour = "mocha",
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
-			transparent_background = true, -- Must be true
-			term_colors = true,
-			dim_inactive = {
-				enabled = false,
-			},
-			styles = {
-				comments = { "italic" },
-				keywords = { "italic" },
-				functions = {},
-				variables = {},
-			},
-			integrations = {
-				treesitter = true,
-				lualine = true,
-				cmp = true,
-				gitsigns = true,
-				telescope = {
-					enabled = true,
-				},
-				which_key = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "underline" },
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-				},
-				mason = true,
-			},
+		config = function()
+			require("0x96f").setup()
+			vim.cmd.colorscheme("0x96f")
 
-			-- 🟢 CUSTOM HIGHLIGHTS (The Source of Truth)
-			custom_highlights = function(colors)
-				return {
-					-- 1. TELESCOPE TRANSPARENCY
-					TelescopeNormal = { bg = "NONE" },
-					TelescopeBorder = { bg = "NONE" },
-					TelescopePromptNormal = { bg = "NONE" },
-					TelescopePromptBorder = { bg = "NONE" },
-					TelescopeResultsNormal = { bg = "NONE" },
-					TelescopeResultsBorder = { bg = "NONE" },
-					TelescopePreviewNormal = { bg = "NONE" },
-					TelescopePreviewBorder = { bg = "NONE" },
+			-- 🟢 FORCE TRANSPARENCY & FIX BORDERS
+			-- Global elements
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+			vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+			vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" }) -- Fix generic floating borders
 
-					-- 2. ORGMODE HEADLINES (Official Catppuccin Palette)
-					-- Using the colors object ensures perfect theme integration
-					OrgHeadlineLevel1 = { fg = colors.red, bold = true },
-					OrgHeadlineLevel2 = { fg = colors.peach, bold = true },
-					OrgHeadlineLevel3 = { fg = colors.yellow, bold = true },
-					OrgHeadlineLevel4 = { fg = colors.green, bold = true },
-					OrgHeadlineLevel5 = { fg = colors.sapphire, bold = true },
-					OrgHeadlineLevel6 = { fg = colors.mauve, bold = true },
-					OrgHeadlineLevel7 = { fg = colors.teal, bold = true },
-					OrgHeadlineLevel8 = { fg = colors.maroon, bold = true },
+			-- Fix Telescope Borders (Remove black background)
+			vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
 
-					-- 3. MARKDOWN HEADLINES (Standard Syntax)
-					markdownH1 = { link = "OrgHeadlineLevel1" },
-					markdownH2 = { link = "OrgHeadlineLevel2" },
-					markdownH3 = { link = "OrgHeadlineLevel3" },
-					markdownH4 = { link = "OrgHeadlineLevel4" },
-					markdownH5 = { link = "OrgHeadlineLevel5" },
-					markdownH6 = { link = "OrgHeadlineLevel6" },
-
-					-- 4. MARKDOWN HEADLINES (Treesitter Syntax)
-					["@markup.heading.1.markdown"] = { link = "OrgHeadlineLevel1" },
-					["@markup.heading.2.markdown"] = { link = "OrgHeadlineLevel2" },
-					["@markup.heading.3.markdown"] = { link = "OrgHeadlineLevel3" },
-					["@markup.heading.4.markdown"] = { link = "OrgHeadlineLevel4" },
-					["@markup.heading.5.markdown"] = { link = "OrgHeadlineLevel5" },
-					["@markup.heading.6.markdown"] = { link = "OrgHeadlineLevel6" },
-				}
-			end,
-		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin-mocha")
+			-- Fix WhichKey Borders
+			vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
+			vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "none" })
 		end,
 	},
 }
