6255c4eefe25f6baa92c8957122d70fc124815bb
MurphLaws <nicolaslassojr@gmail.com>
2026-06-25 18:22:29 -0500

    feat(02-01): configure tunnelvision.nvim plugin with lazy-loaded toggle
    - Create nvim/.config/nvim/lua/illico/plugins/tunnelvision.lua
    - Plugin: leolaurindo/tunnelvision.nvim
    - Keybinding: <leader>tv → :TunnelVision toggle (lazy-loaded)
    - desc: 'TunnelVision toggle' for which-key display
    - opts = {} to trigger setup() with defaults
    - Follows existing snacks/leap plugin patterns

---
+++ nvim/.config/nvim/lua/illico/plugins/tunnelvision.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"leolaurindo/tunnelvision.nvim",
+		opts = {},
+		keys = {
+			{
+				"<leader>tv",
+				"<cmd>TunnelVision toggle<CR>",
+				desc = "TunnelVision toggle",
+			},
+		},
+	},
+}
